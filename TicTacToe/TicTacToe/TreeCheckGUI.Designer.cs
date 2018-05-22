a3cfda39f141f48754cf80cbadd667e4e50e5d2c
semi10 <f.semion89@gmail.com>
2018-05-22 16:13:06 +0300

    Draw tree class created

--- TicTacToe/TicTacToe/TreeCheckGUI.Designer.cs
+++ TicTacToe/TicTacToe/TreeCheckGUI.Designer.cs
@@ -28,22 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.DrawTreebtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // DrawTreebtn
+            // 
+            this.DrawTreebtn.Location = new System.Drawing.Point(93, 40);
+            this.DrawTreebtn.Name = "DrawTreebtn";
+            this.DrawTreebtn.Size = new System.Drawing.Size(75, 23);
+            this.DrawTreebtn.TabIndex = 0;
+            this.DrawTreebtn.Text = "Draw Tree";
+            this.DrawTreebtn.UseVisualStyleBackColor = true;
+            this.DrawTreebtn.Click += new System.EventHandler(this.DrawTreebtn_Click);
             // 
             // TreeCheckGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(700, 573);
+            this.ClientSize = new System.Drawing.Size(257, 152);
+            this.Controls.Add(this.DrawTreebtn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "TreeCheckGUI";
             this.Text = "TreeCheck";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.TreeCheckGUI_Paint);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button DrawTreebtn;
     }
 }
