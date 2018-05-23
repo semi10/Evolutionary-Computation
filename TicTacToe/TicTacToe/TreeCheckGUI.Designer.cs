14ca57bbfbdcb9c24c5493e7193c98a14623c011
semi10 <f.semion89@gmail.com>
2018-05-23 11:08:59 +0300

    First GUI draw trial

--- TicTacToe/TicTacToe/TreeCheckGUI.Designer.cs
+++ TicTacToe/TicTacToe/TreeCheckGUI.Designer.cs
@@ -38,6 +38,7 @@
             this.Name = "TreeCheckGUI";
             this.Text = "TreeCheck";
             this.Load += new System.EventHandler(this.TreeCheckGUI_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.TreeCheckGUI_Paint);
             this.ResumeLayout(false);
 
         }
--- TicTacToe/TicTacToe/TreeCheckGUI.cs
+++ TicTacToe/TicTacToe/TreeCheckGUI.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TicTacToe
@@ -14,10 +8,20 @@ namespace TicTacToe
     {
         private const int MAX_DEPTH = 5;
         int indentation = 0;
+        Tree mainTree;
 
         public TreeCheckGUI()
         {
             InitializeComponent();
+        }
+
+        private void TreeCheckGUI_Load(object sender, EventArgs e)
+        {
+            mainTree = new Tree();
+            mainTree.growRandomTree(MAX_DEPTH);
+
+            Node currentNode = mainTree.getRoot();
+            drawTreeConsole(currentNode);
         }
 
         private void drawTreeConsole(Node currentNode)
@@ -36,13 +40,50 @@ namespace TicTacToe
             indentation--;
         }
 
-        private void TreeCheckGUI_Load(object sender, EventArgs e)
+        private void TreeCheckGUI_Paint(object sender, PaintEventArgs e)
         {
-            Tree mainTree = new Tree();
-            mainTree.growRandomTree(MAX_DEPTH);
+            Point point1 = new Point(Size.Width / 2, 0);
+            Point point2 = new Point(Size.Width / 3, 70);
+            Point point3 = new Point((Size.Width / 3) * 2, 70);
 
-            Node currentNode = mainTree.getRoot();
-            drawTreeConsole(currentNode);
+            Console.WriteLine(Size.Width);
+
+            int size = 16;
+            Size radius = new Size(size, size);
+            Color color = Color.Blue;
+
+            DrawEllipse(e, point1, radius, color);
+            DrawString(e, "1", point1, 9);
+            DrawEllipse(e, point2, radius, color);
+            DrawString(e, "2", point2, 9);
+            DrawEllipse(e, point3, radius, color);
+            DrawString(e, "3", point3, 9);
+
+            DrawLine(e, point1, point2);
+            DrawLine(e, point1, point3);
         }
+
+        private void DrawEllipse(PaintEventArgs e, Point position, Size radius, Color color)
+        {
+            SolidBrush brush = new SolidBrush(color);
+            Rectangle circle = new Rectangle(position, radius);
+            e.Graphics.FillEllipse(brush, circle);
+            // formGraphics.Dispose();
+        }
+
+        private void DrawLine(PaintEventArgs e, Point begin, Point end)
+        {
+            Pen blackPen = new Pen(Color.Black, 2);
+            e.Graphics.DrawLine(blackPen, begin, end);
+        }
+
+        public void DrawString(PaintEventArgs e, String drawString, Point position, int size)
+        {
+            Font drawFont = new Font("Arial", size);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+            PointF drawPoint = new PointF(150.0F, 150.0F);
+            e.Graphics.DrawString(drawString, drawFont, drawBrush, position);
+        }
+
     }
 }
